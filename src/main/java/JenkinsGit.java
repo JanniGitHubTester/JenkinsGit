357a0e554c2d77d0beadf0056e18e7b0540941b9
CHIRMI <i.chirmpos@gmail.com>
2019-03-28 16:09:38 +0100

    Add hello GitHub message

--- src/main/java/JenkinsGit.java
+++ src/main/java/JenkinsGit.java
@@ -1,7 +1,10 @@
-package main.java;
+
 
 public class JenkinsGit {
     public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+        System.out.println("Hello GitHub!");
+        System.out.println("Hello GitHub!");
         System.out.println("Hello GitHub!");
         System.out.println("Hello GitHub!");
         System.out.println("Hello GitHub!");
