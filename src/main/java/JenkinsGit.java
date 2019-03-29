76e35a679738e0144404de46897804f3616b3bc0
CHIRMI <i.chirmpos@gmail.com>
2019-03-29 08:56:52 +0100

    Add Fail

--- src/main/java/JenkinsGit.java
+++ src/main/java/JenkinsGit.java
@@ -12,6 +12,6 @@ public class JenkinsGit {
         System.out.println("Hello Test!");
         System.out.println("Hello Test GitHub!");
         System.out.println("Hello Test GitHub!");
-        System.out.println("Hello Test GitHub!");
+        System.out.println("Hello Test GitHub!";
     }
 }
