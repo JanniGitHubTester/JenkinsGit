1d578a2d562b869d11e6c695123b1df1535509a5
JanniGitHubTester <49028151+JanniGitHubTester@users.noreply.github.com>
2019-03-29 09:06:19 +0100

    Update JenkinsGit.java

--- src/main/java/JenkinsGit.java
+++ src/main/java/JenkinsGit.java
@@ -3,15 +3,5 @@
 public class JenkinsGit {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
-        System.out.println("Hello GitHub!");
-        System.out.println("Hello GitHub!");
-        System.out.println("Hello GitHub!");
-        System.out.println("Hello GitHub!");
-        System.out.println("Hello GitHub!");
-        System.out.println("Hello GitHub!");
-        System.out.println("Hello Test!");
-        System.out.println("Hello Test GitHub!");
-        System.out.println("Hello Test GitHub!");
-        System.out.println("Hello Test GitHub!");
     }
 }
